55e5ebd3b188d81dceef55bf8b12e0d606bf1f84
crazyoptimist <crazyoptimist@mail.com>
2020-11-16 21:09:43 +0000

    :alien::memo:Use the original code for config, modify credit info

--- config/initializers/slack_ruby_bot_server.rb
+++ config/initializers/slack_ruby_bot_server.rb
@@ -1,3 +1,5 @@
 SlackRubyBotServer.configure do |config|
   config.view_paths << File.expand_path(File.join(__dir__, '../../public'))
+  config.oauth_version = :v2
+  config.oauth_scope = ENV.fetch('SLACK_OAUTH_SCOPE').split(/\s*,\s*/)
 end
